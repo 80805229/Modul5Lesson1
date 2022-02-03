69e3658f87923cc8f32678fa5fe7afb3f2484d5b
Anna <aanikonova15@gmail.com>
2022-02-03 19:54:44 +0300

    Add Example004_Random

---
+++ Example004_Random/Program.cs
@@ -0,0 +1,8 @@
+﻿double numberA = new Random().Next (1,5);
+Console.Write("Делимое ");
+Console.WriteLine(numberA);
+double numberB = new Random().Next (1,5);
+Console.Write("Делитель ");
+Console.WriteLine(numberB);
+Console.Write("Частное равно ");
+Console.WriteLine(numberA / numberB);
