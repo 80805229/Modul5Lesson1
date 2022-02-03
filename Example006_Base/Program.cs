6f299b72da891504b9c210ba602c6cbf7a3fd302
Anna <aanikonova15@gmail.com>
2022-02-03 21:01:42 +0300

    Add Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 2;
+int b = 6;
+int c = 9;
+int d = 1;
+int e = 4;
+
+int max = a;
+
+if (max < a) max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+
+Console.Write("Максимальное число равно ");
+Console.WriteLine(max);
