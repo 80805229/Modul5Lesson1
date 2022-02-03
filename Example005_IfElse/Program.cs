ededdc89bc6db2dd132476365059b4f6ffe1aa25
Anna <aanikonova15@gmail.com>
2022-02-03 20:33:13 +0300

    Add Example005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите Ваше имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "аня")
+{
+    Console.WriteLine("Ура, это же АНЯ!");
+}
+else
+{
+    Console.Write("Здравствуй, ");
+    Console.Write(username);
+}
